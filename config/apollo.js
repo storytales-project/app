c0d3e6b40e04d2531dced9376fe8a5af2c90936e
yehezkielt <yehezkieltatang1@gmail.com>
2024-05-20 19:14:31 +0700

    wiring register

--- config/apollo.js
+++ config/apollo.js
@@ -3,7 +3,7 @@ import { setContext } from '@apollo/client/link/context';
 import * as SecureStore from 'expo-secure-store';
 
 const httpLink = createHttpLink({
-    uri: 'https://tumblr.tatang.online/',
+    uri: 'https://finalproject.daffabaihaqi.online/graphql',
 });
 
 const authLink = setContext(async (_, { headers }) => {
