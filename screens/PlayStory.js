86ca55d2ba2b63b2e76ca01d21db28e172df4341
yehezkielt <yehezkieltatang1@gmail.com>
2024-05-21 11:02:47 +0700

    add backsound

--- screens/PlayStory.js
+++ screens/PlayStory.js
@@ -12,6 +12,7 @@ const PlayStory = ({ navigation }) => {
     const [playSeconds, setPlaySeconds] = useState(0);
     const [duration, setDuration] = useState(0);
     const [sound, setSound] = useState(null);
+    const [backsound, setBacksound] = useState(null);
     const [sliderEditing, setSliderEditing] = useState(false);
 
     const onPlayStory = async () => {
@@ -20,9 +21,11 @@ const PlayStory = ({ navigation }) => {
         } else {
             if (playState === 'paused') {
                 await sound.playAsync();
+                await backsound.playAsync();
                 setPlayState('playing');
             } else {
                 await sound.pauseAsync();
+                await backsound.pauseAsync();
                 setPlayState('paused');
             }
         }
@@ -32,15 +35,37 @@ const PlayStory = ({ navigation }) => {
         // Implement this function
     };
 
+    // untuk cerah
+    // { uri: "https://ik.imagekit.io/yehezkielt/Swans%20In%20Flight%20-%20Asher%20Fulero.mp3?updatedAt=1716263600233" },
+    // { shouldPlay: true, volume: 0.1 },
+
+    // untuk fun
+    // { uri: "https://ik.imagekit.io/yehezkielt/No.9_Esther_s%20Waltz%20-%20Esther%20Abrami.mp3?updatedAt=1716262251733" },
+    // { shouldPlay: true, volume: 0.1 },
+
+    // untuk sedih 
+    // { uri: "https://ik.imagekit.io/yehezkielt/Wistful%20Harp%20-%20Andrew%20Huang.mp3?updatedAt=1716262255299" },
+    // { shouldPlay: true, volume: 0.2 },
+
+    // untuk thriller
+    // { uri: "https://ik.imagekit.io/yehezkielt/Bourree%20-%20Joel%20Cummins.mp3?updatedAt=1716262399068" },
+    // { shouldPlay: true, volume: 0.1 },
+
     const play = async () => {
         try {
             const { sound: playbackObject } = await Audio.Sound.createAsync(
                 { uri: "https://res.cloudinary.com/dp9n1icsc/video/upload/v1715833322/dlrmny8n73d9ws06l8h8.mp3" },
                 { shouldPlay: true }
             );
+            const { sound: playbackBacksound } = await Audio.Sound.createAsync(
+                { uri: "https://ik.imagekit.io/yehezkielt/Bourree%20-%20Joel%20Cummins.mp3?updatedAt=1716262399068" },
+                { shouldPlay: true, volume: 0.1 },
+            );
 
             setSound(playbackObject);
+            setBacksound(playbackBacksound);
             playbackObject.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
+            playbackBacksound.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
             setPlayState('playing');
         } catch (error) {
             console.error('Error loading sound:', error);
@@ -61,6 +86,7 @@ const PlayStory = ({ navigation }) => {
 
     const onReset = () => {
         if (sound) {
+            backsound.stopAsync()
             sound.stopAsync();
             setPlayState('paused');
             setPlaySeconds(0);
